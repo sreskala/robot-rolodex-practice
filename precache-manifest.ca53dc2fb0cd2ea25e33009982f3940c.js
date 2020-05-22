004b0fcbbf932d44f34b75679e0141ba18984c41
sreskala <sam.sagas@hotmail.com>
2020-05-22 14:10:42 -0400

    Updates

---
+++ precache-manifest.ca53dc2fb0cd2ea25e33009982f3940c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4131cc41a8428a5fed854829b8d20a50",
+    "url": "/robot-rolodex-practice/index.html"
+  },
+  {
+    "revision": "aca46ae9fa5f9cba8325",
+    "url": "/robot-rolodex-practice/static/css/main.f95e7a40.chunk.css"
+  },
+  {
+    "revision": "de9a4b61cb1b5dc8dc8f",
+    "url": "/robot-rolodex-practice/static/js/2.27b39556.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robot-rolodex-practice/static/js/2.27b39556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aca46ae9fa5f9cba8325",
+    "url": "/robot-rolodex-practice/static/js/main.366a2163.chunk.js"
+  },
+  {
+    "revision": "7b4dd5579aaf2cb81b8a",
+    "url": "/robot-rolodex-practice/static/js/runtime-main.289507ac.js"
+  }
+]);
